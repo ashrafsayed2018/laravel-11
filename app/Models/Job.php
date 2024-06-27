a0e5da7535f281333e81756b20b3ab94a6f667c5
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2024-06-27 15:08:45 +0300

    ep 11 eloquet relationship hasMany and belognsTo

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model {
 
@@ -12,5 +13,9 @@ class Job extends Model {
     protected $table = "job_listings";
 
     protected $fillable = ['title', 'location', 'salary'];
+
+    public function employer():BelongsTo {
+        return $this->belongsTo(Employer::class);
+    }
 
 }
